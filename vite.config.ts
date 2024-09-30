5c5167d160396cd7281a35c4b06dd84c223841c4
Joshua Sunday <joshuasunday300@gmail.com>
2024-09-30 18:36:31 +0100

    buffer

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
+  },
   resolve: {
     alias: {
       crypto: 'crypto-js',
+      stream: 'stream-browserify', // If stream needs polyfilling
+      buffer: 'buffer',
     }
   }
 })
+
+
