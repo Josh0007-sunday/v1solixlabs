751d8e061cec9e080d4db0a6bb24f615b8a650a5
Joshua Sunday <joshuasunday300@gmail.com>
2024-10-01 10:27:01 +0100

    process

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: false, // Disable source maps in production
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
@@ -15,37 +15,38 @@ export default defineConfig({
         },
       },
     },
-    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit to 1000 KB
+    chunkSizeWarningLimit: 1000,
   },
   optimizeDeps: {
     include: ['@solana/spl-token', 'crypto-js', '@walletconnect/qrcode-modal'],
     esbuildOptions: {
-      // Enable esbuild polyfill plugins
       plugins: [
         NodeGlobalsPolyfillPlugin({
           buffer: true,
         }),
       ],
-      // Define globalThis and Buffer to avoid reference errors
       define: {
         global: 'globalThis',
+        process: JSON.stringify({
+          env: {}, // Polyfill `process.env` with empty object or your env variables
+        }),
       },
     },
   },
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.flexlend.fi', // Proxy API calls to Flexlend API
-        changeOrigin: true, // Change the origin of the request to the target URL
-        rewrite: (path) => path.replace(/^\/api/, ''), // Rewrite /api to empty
+        target: 'https://api.flexlend.fi',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
   resolve: {
     alias: {
-      stream: 'stream-browserify', // Polyfill stream for browser
-      buffer: 'buffer', // Polyfill buffer
-      crypto: 'crypto.js', // Ensure crypto is mapped to crypto.js in browser
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+      crypto: 'crypto.js',
     },
   },
 });
