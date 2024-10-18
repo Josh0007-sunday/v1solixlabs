f50e1de717bb3020bb4c3df07f9ab97cae36059c
Joshua Sunday <joshuasunday300@gmail.com>
2024-10-18 16:27:11 +0100

    remove unused declaration

--- src/landingpage/header/page.tsx
+++ src/landingpage/header/page.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { useWallet } from "@solana/wallet-adapter-react";
